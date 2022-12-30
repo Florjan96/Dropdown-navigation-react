0030e7e4622a03c2ad6c1921532a03f81ce71ad5
Florjan96 <florianshiqerukaj@yahoo.com>
2022-12-30 19:26:57 +0100

    Created the structure of layout

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,36 @@ function App() {
 
   return (
     <div className="App">
-      
+      <div className="main">
+
+      <div className="header">
+ 
+ <div className="header-left">
+  <h1>snap</h1>
+ </div>
+
+ <div className="header-middle">
+  <ul>
+    <li>Features</li>
+    <li>Company</li>
+    <li>Careers</li>
+    <li>About</li>
+  </ul>
+ </div>
+ 
+ <div className="header-right">
+  <h4>Login</h4>
+  <h4>Register</h4>
+ </div>
+
+      </div>
+    
+    <div className="left-side">
+<h1>left</h1>
+    </div>
+    
+    <div className="right-side">Rigt</div>
+    </div>
     </div>
   )
 }
